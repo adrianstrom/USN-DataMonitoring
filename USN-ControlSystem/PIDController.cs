e6662648e5b6868797e4624aa118955e61ad5afc
Adrian Strom <nostroma@krohne.com>
2022-04-28 21:17:45 +0200

    Control System proj.

---
+++ USN-ControlSystem/PIDController.cs
@@ -0,0 +1,16 @@
+﻿namespace USN_ControlSystem
+{
+    public class PIDController
+    {
+        private double _kp;
+        private double _ti;
+        private double _td;
+
+        public PIDController(double Kp, double Ti, double Td)
+        {
+            _kp = Kp;
+            _ti = Ti;
+            _td = Td;
+        }
+    }
+}
