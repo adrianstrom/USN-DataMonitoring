0d7a1befc9e29d15c84725f4e2233f6af5f417e1
Adrian Strom <nostroma@krohne.com>
2022-04-28 22:56:34 +0200

    pid

---
+++ USN-ControlSystem/Controllers/AirHeaterModel.cs
@@ -0,0 +1,16 @@
+﻿namespace USN_ControlSystem.Controllers
+{
+    public class AirHeaterModel
+    {
+        public double OutletTemperature { get; set; }
+        public double EnvironmentTemperature { get; set; }
+        public double ControlSignal { get; set; }
+        public double HeaterGain { get; set; }
+
+
+        public AirHeaterModel()
+        {
+
+        }
+    }
+}
--- USN-ControlSystem/Controllers/PIDController.cs
+++ USN-ControlSystem/Controllers/PIDController.cs
@@ -6,10 +6,14 @@
         public double Ti { get; set; }
         public double Td { get; set; }
 
+        public double TimeStep { get; set; }
+
         public bool Manual { get; set; }
 
         public double SetPoint { get; set; }
         public double MeasuredValue { get; set; }
+
+        public double ControlSignal { get; set; }
 
         public double Error => SetPoint - MeasuredValue;
 
@@ -18,6 +22,19 @@
             this.Kp = Kp;
             this.Ti = Ti;
             this.Td = Td;
+        }
+
+        public double GetControlSignal()
+        {
+            if (Manual)
+            {
+                return ControlSignal;
+            }
+            var lastprocessvalue = 0;
+            var u_p = Kp * Error;
+            var u_i = lastprocessvalue + (Kp / Ti) * TimeStep * Error;
+            // TODO: u_d (deriviate term)
+            return u_p + u_i;
         }
     }
 }
